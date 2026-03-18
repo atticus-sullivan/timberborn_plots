ecf750a128c4c2c9eb63bc5711236c002df42b61
Lukas <oss.heindl+git@protonmail.com>
2026-03-18 12:17:29 +0100

    refactor and generalize the code

--- analysis.py
+++ analysis.py
@@ -2,122 +2,150 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-game_name = "Plankshire"
-game_dir = f"/home/lukas/.local/share/Steam/steamapps/compatdata/1062090/pfx/drive_c/users/steamuser/Documents/Timberborn/Saves/{game_name}/"
-
+import os
+from argparse import ArgumentParser
 import json
 import zipfile
 from collections import Counter
 import glob
-import os
+from pathlib import Path
 
-OUTPUT_PATH = f"./{game_name}.analysis.json"
+#######
+# CLI #
+#######
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("savefile_path", type=Path)
+    parser.add_argument("game_name")
+    return parser.parse_args()
+
+#########
+# UTILS #
+#########
 
 def coord2tuple(v):
     return (v['X'], v['Y'], v['Z'])
 
-recipes_path = "./recipes.json"
-with open(recipes_path) as f:
-    recipes = json.load(f)
-
-def latest_save(dir):
-    list_of_files = glob.glob(game_dir+"*.timber")
+def latest_save(dir: Path):
+    list_of_files = dir.glob("*.timber")
     latest_file = max(list_of_files, key=os.path.getctime)
     return latest_file
-path = latest_save(game_dir)
-print(f"Select '{path}' as savefile")
 
-with zipfile.ZipFile(path) as z:
-    with z.open("world.json") as f:
-        world = json.load(f)
+#########
+# LOGIC #
+#########
 
-assert world['GameVersion'] == "1.0.12.5-3db367b-sw", f"Unknwon gameversion {world['GameVersion']} seen"
+def obtain_stats(world):
+    # count active buildings
+    # active_templates = Counter()
 
-planting_area = {
-    t: set(map(coord2tuple, vs))
-    for (t,vs) in world['Singletons']['PlantingService']['PlantingMap'].items()
-}
-cutting_area = set(map(coord2tuple, world['Singletons']['TreeCuttingArea']['CuttingArea']))
+    # count the active recipes
+    active_recipes = Counter()
+    for e in world["Entities"]:
+        template = e.get("Template")
+        if not template or template not in recipes:
+            continue
 
-# # counter for building types
-# template_counts = Counter(
-#     e["Template"]
-#     for e in world["Entities"]
-#     if "Template" in e
-# )
+        comps = e.get("Components", {})
 
+        finished = comps.get("BlockObjectState", {}).get("Finished", True)
+        paused   = comps.get("PausableBuilding", {}).get("Paused", False)
 
-# only count active buildings
-active_templates = Counter()
-active_recipes = Counter()
-for e in world["Entities"]:
-    template = e.get("Template")
-    if not template or template not in recipes:
-        continue
+        if not finished or paused:
+            continue
+        # active_templates[template] += 1
 
-    comps = e.get("Components", {})
+        manufactory = comps.get("Manufactory")
 
-    finished = comps.get("BlockObjectState", {}).get("Finished", True)
-    paused   = comps.get("PausableBuilding", {}).get("Paused", False)
+        if manufactory:
+            recipe_name_init = manufactory.get("CurrentRecipe", "default")
+        else:
+            recipe_name_init = "default"
 
-    if not finished or paused:
-        continue
-    active_templates[template] += 1
+        if template in recipes:
+            # logic to obtain the correct recipe, especially if the entity does not specify the recipe explicitly
+            for recipe_name in [recipe_name_init, *map(lambda x: x[0], filter(lambda x: x[1].get("canFallbackTo", False), recipes[template]['recipes'].items()))]:
+                recipe = recipes[template]['recipes'][recipe_name]
+                coord = coord2tuple(comps.get('BlockObject', {}).get('Coordinates', {"X":0, "Y":0, "Z": 0}))
+                skip = False
+                if recipe.get('needsCuttingArea', False) and coord not in cutting_area:
+                    skip = True
+                if recipe.get('needsPlantingArea', False) and coord not in planting_area.get(template, {}):
+                    skip = True
+                if (g := recipe.get('needsGrowthProgress', None)) and g and comps.get("Growable", {}).get("GrowthProgress", 0) < g:
+                    skip = True
 
-    manufactory = comps.get("Manufactory")
+                if skip: continue
+                # use the first recipe which is not skipped
+                break
+            else: continue # executed if the for-loop terminates normally (no break)
+        else:
+            # use the initial recipe to avoid crashes
+            recipe_name = recipe_name_init
 
-    if manufactory:
-        recipe_name_init = manufactory.get("CurrentRecipe", "default")
-    else:
-        recipe_name_init = "default"
+        active_recipes[(template, recipe_name)] += 1
+    return active_recipes
 
-    if template in recipes:
-        for recipe_name in [recipe_name_init, *map(lambda x: x[0], filter(lambda x: x[1].get("canFallbackTo", False), recipes[template]['recipes'].items()))]:
-            recipe = recipes[template]['recipes'][recipe_name]
-            coord = coord2tuple(comps.get('BlockObject', {}).get('Coordinates', {"X":0, "Y":0, "Z": 0}))
-            skip = False
-            if recipe.get('needsCuttingArea', False) and coord not in cutting_area:
-                skip = True
-            if recipe.get('needsPlantingArea', False) and coord not in planting_area.get(template, {}):
-                skip = True
-            if (g := recipe.get('needsGrowthProgress', None)) and g and comps.get("Growable", {}).get("GrowthProgress", 0) < g:
-                skip = True
+def generate_hut_output(recipes, active_recipes):
+    huts = []
+    for template, template_data in recipes.items():
+        recipes = template_data['recipes']
 
-            if skip: continue
-            break
-        else: continue # executed if the for-loop terminates normally (no break)
-    else:
-        recipe_name = recipe_name_init
+        for recipe_name,recipe in recipes.items():
+            cnt = active_recipes.get((template, recipe_name), 0)
+            if cnt <= 0: continue
 
-    active_recipes[(template, recipe_name)] += 1
+            dur = recipe.get("duration", "0s")
+            huts.append({
+                "template": template,
+                "name": template,
+                "recipe": recipe_name if recipe_name != "default" else None,
+                "cnt": cnt,
+                "dur": dur,
+                "inputs": recipe.get("inputs", {}),
+                "outputs": recipe.get("outputs", {})
+            })
+    return huts
 
-# artificially added buildings
-active_recipes[("Food", "default")] += 1
+########
+# MAIN #
+########
+if __name__ == '__main__':
+    # init
+    args = parse_args()
+    output_path = f"./{args.game_name}.analysis.json"
+    path = latest_save(args.savefile_path / args.game_name)
+    print(f"Select '{path}' as savefile")
 
-# print(active_templates)
+    # read input files
+    recipes_path = "./recipes.json"
+    with open(recipes_path) as f:
+        recipes = json.load(f)
 
-# collect huts
-huts = []
-for template, template_data in recipes.items():
-    recipes = template_data['recipes']
+    with zipfile.ZipFile(path) as z:
+        with z.open("world.json") as f:
+            world = json.load(f)
 
-    for recipe_name,recipe in recipes.items():
-        cnt = active_recipes.get((template, recipe_name), 0)
-        if cnt <= 0: continue
+    # ensure the recipes match the version of the game
+    # TODO: make this work with more recent versions / matrix with version -> recipe
+    assert world['GameVersion'] == "1.0.12.5-3db367b-sw", f"Unknwon gameversion {world['GameVersion']} seen"
 
-        dur = recipe.get("duration", "0s")
-        huts.append({
-            "template": template,
-            "name": template,
-            "recipe": recipe_name if recipe_name != "default" else None,
-            "cnt": cnt,
-            "dur": dur,
-            "inputs": recipe.get("inputs", {}),
-            "outputs": recipe.get("outputs", {})
-        })
+    # obtain the coordinates of the planting-/cutting-area
+    planting_area = {
+        t: set(map(coord2tuple, vs))
+        for (t,vs) in world['Singletons']['PlantingService']['PlantingMap'].items()
+    }
+    cutting_area = set(map(coord2tuple, world['Singletons']['TreeCuttingArea']['CuttingArea']))
 
-# write output
-with open(OUTPUT_PATH, "w") as f:
-    json.dump(huts, f, indent=2)
+    active_recipes = obtain_stats(world)
+    # artificially added buildings
+    active_recipes[("Food", "default")] += 1
 
-print(f"Wrote {OUTPUT_PATH}")
+    huts = generate_hut_output(recipes, active_recipes)
+
+    # write output
+    with open(output_path, "w") as f:
+        json.dump(huts, f, indent=2)
+
+    print(f"Wrote {output_path}")
